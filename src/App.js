26f7602fd32ed7204fa34dffbaa0b7fedfafe77e
andyk <andy.keh@gmail.com>
2021-02-27 15:02:26 -0600

    draft upload for review

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from "./pages/dashboard"
+import Exchange from "./pages/exchange"
+import Allocation from "./pages/allocation"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/exchange">
+            <Exchange />
+          </Route>
+          <Route path="/allocation">
+          <Allocation/>
+          </Route>
+        </Switch>
+        </Router>
     </div>
   );
 }
